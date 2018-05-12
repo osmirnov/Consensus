2007a0c91f61700348c12e1b49f2fe8a28d8d6ca
Oleg Smirnov <smirnov.os@gmail.com>
2018-05-12 10:55:26 -0400

    initial cut of FastBFT

---
+++ Consensus.FastBFT/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Consensus.FastBFT
+{
+    class Program
+    {
+        class Replica
+        {
+            public int id;
+            public Tee tee;
+        }
+
+        class PrimaryReplica
+        {
+            public int id;
+            public PrimaryTee tee;
+        }
+
+        class Block
+        {
+            public string id;
+            public int h;
+            public int k;
+            public int i;
+            public List<int> txs = new List<int>();
+            public int sig;
+
+            public Block(int h)
+            {
+                this.h = h;
+            }
+        }
+
+        class Interval
+        {
+            public DateTime from = DateTime.Now;
+            public DateTime to = DateTime.Now;
+        }
+
+        const int clientsCount = 12;
+        const int replicasCount = 6;
+        const int minTransactionsCountInBlock = 10;
+        const int intervalBetweenTransactions = 2; // seconds
+        private const int minNetworkLatency = 10;
+        private const int maxNetworkLatency = 100;
+        static int[] clientIds = Enumerable.Range(0, clientsCount).ToArray();
+        static int[] replicaIds = Enumerable.Range(0, replicasCount).ToArray();
+
+        static ConcurrentQueue<int> transactionPool = new ConcurrentQueue<int>();
+        static ConcurrentDictionary<string, Interval> consensusIntervals = new ConcurrentDictionary<string, Interval>();
+        static Random rnd = new Random(Environment.TickCount);
+
+        static void Main(string[] args)
+        {
+            var from = DateTime.Now;
+
+            using (var cancellationTokenSource = new CancellationTokenSource())
+            {
+                var token = cancellationTokenSource.Token;
+
+                RunClients(token);
+                RunReplicas(token);
+
+                Console.ReadKey();
+                // cancellationTokenSource.Cancel();
+            }
+
+            var to = DateTime.Now;
+
+            Thread.Sleep(2500);
+
+            var intervals = consensusIntervals
+                .Select(ci => ci.Value)
+                .Where(i => (i.to - i.from).TotalSeconds > 3)
+                .OrderBy(i => i.to - i.from)
+                .ToList();
+            var minInterval = intervals.FirstOrDefault();
+            var maxInterval = intervals.LastOrDefault();
+            var avgInterval = intervals.Sum(i => (i.to - i.from).TotalSeconds) / intervals.Count;
+
+            Console.WriteLine($"The time spent on emulation was {(to - from).TotalSeconds}s");
+            //Console.WriteLine($"The time between transactions was {intervalBetweenTransactions}s");
+            //Console.WriteLine($"The time for after speaker election was {waitTimeAfterSpeakerElection}ms");
+            //Console.WriteLine($"Avg network latency was {(maxNetworkLatency - minNetworkLatency) / 2}ms");
+            //Console.WriteLine($"The consesus were reached {intervals.Count}");
+
+            if (minInterval != null)
+                Console.WriteLine($"The min consensus took {(minInterval.to - minInterval.from).TotalSeconds}s");
+
+            if (maxInterval != null)
+                Console.WriteLine($"The max consensus took {(maxInterval.to - maxInterval.from).TotalSeconds}s");
+
+            Console.WriteLine($"The avg consensus took {avgInterval}s");
+
+            Console.ReadKey();
+        }
+
+        private static void EmulateNetworkLatency()
+        {
+            Thread.Sleep(rnd.Next(minNetworkLatency, maxNetworkLatency));
+        }
+
+        private static void RunClients(CancellationToken token)
+        {
+            for (var i = 0; i < clientIds.Length; i++)
+            {
+                var clientId = clientIds[i];
+
+                Task.Factory.StartNew(() =>
+                {
+                    while (token.IsCancellationRequested == false)
+                    {
+                        GenerateTransaction();
+                    }
+                }, token, TaskCreationOptions.LongRunning, TaskScheduler.Current);
+            }
+        }
+
+        private static void GenerateTransaction()
+        {
+            var sec = DateTime.Now.Second;
+            if (sec % 2 == rnd.Next(1) && sec % intervalBetweenTransactions == 0)
+            {
+                var tx = rnd.Next();
+                transactionPool.Enqueue(tx);
+                Console.WriteLine($"The transaction #{tx} was generated");
+
+                // transaction was generated -> propagate this to network
+                EmulateNetworkLatency();
+            }
+        }
+
+        private static void RunReplicas(CancellationToken token)
+        {
+            var workingReplicaIds = replicaIds
+                .OrderBy(r => rnd.Next(replicaIds.Length))
+                .Take(replicaIds.Length * 2 / 3)
+                .ToArray();
+
+            var faultyReplicaIds = replicaIds
+                .Except(workingReplicaIds)
+                .ToArray();
+
+            var activeReplicaIds = workingReplicaIds
+                .Take(workingReplicaIds.Length * 2 / 3)
+                .ToArray();
+
+            var passiveReplicaIds = workingReplicaIds
+                .Except(activeReplicaIds)
+                .ToArray();
+
+            var primaryReplicaId = activeReplicaIds.First();
+
+            var secondaryReplicas = workingReplicaIds
+                .Where(rid => rid != primaryReplicaId)
+                .Select(rid =>
+                {
+                    var replica = new Replica();
+                    replica.id = rid;
+                    replica.tee = new Tee();
+                    replica.tee.isActive = activeReplicaIds.Contains(rid);
+                    return replica;
+                })
+                .ToArray();
+
+            var leftTree = DiscoverReplicaTopology(null, secondaryReplicas.Take(secondaryReplicas.Count() / 2));
+            var rightTree = DiscoverReplicaTopology(null, secondaryReplicas.Skip(secondaryReplicas.Count() / 2));
+
+            var primaryReplica = new PrimaryReplica();
+            primaryReplica.id = primaryReplicaId;
+            var tree = new TreeNode { leftNode = leftTree, rightNode = rightTree, replica = primaryReplicaId };
+            primaryReplica.tee = new PrimaryTee(tree);
+
+            for (var i = 0; i < secondaryReplicas.Length; i++)
+            {
+                var secondaryReplica = secondaryReplicas[i];
+
+                Task.Factory.StartNew(() =>
+                {
+                    while (token.IsCancellationRequested == false)
+                    {
+                        // 
+                    }
+                }, token, TaskCreationOptions.LongRunning, TaskScheduler.Current);
+            }
+        }
+
+        private static TreeNode DiscoverReplicaTopology(TreeNode parentNode, IEnumerable<Replica> secondaryReplicas)
+        {
+            var parentReplica = secondaryReplicas.FirstOrDefault();
+            if (parentReplica == null) return null;
+
+            var childReplicas = secondaryReplicas.Except(new[] { parentReplica });
+            var tree = new TreeNode();
+
+            tree.parentNode = parentNode;
+            tree.leftNode = DiscoverReplicaTopology(tree, childReplicas.Take(childReplicas.Count() / 2));
+            tree.rightNode = DiscoverReplicaTopology(tree, childReplicas.Skip(childReplicas.Count() / 2));
+            tree.replica = parentReplica.id;
+
+            return tree;
+        }
+    }
+}
---
+++ Consensus.FastBFT/Tee.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Consensus.FastBFT
+{
+    public class Tee
+    {
+        public readonly string publicKey;
+        protected readonly string privateKey;
+
+        public uint counterLatest; // all replicas -> latest counter
+        public uint viewNumber; // all replicas -> current view number
+
+        public byte viewKey; // active replica -> current view key agreed with the primary
+
+        public bool isActive;
+
+        public Tee()
+        {
+            // fake public/private keys
+            publicKey = Guid.NewGuid().ToString("N");
+            privateKey = publicKey;
+        }
+
+        public string Encrypt(string str)
+        {
+            return publicKey + str;
+        }
+
+        public byte[] EncryptAuth(byte[] buffer, byte? viewKey = null)
+        {
+            return (new byte[] { (viewKey ?? this.viewKey), 0xff })
+                .Concat(buffer)
+                .ToArray();
+        }
+
+        public string Decrypt(string encryptedStr)
+        {
+            return encryptedStr.Substring(encryptedStr.IndexOf(publicKey), publicKey.Length);
+        }
+
+        public byte[] DecryptAuth(byte[] encryptedBuffer, byte? viewKey = null)
+        {
+            var key = (viewKey ?? this.viewKey);
+
+            if (encryptedBuffer[0] != key) throw new Exception("Invalid encryption");
+            if (encryptedBuffer[1] != 0xff) throw new Exception("Invalid encryption");
+
+            return encryptedBuffer.Skip(2).ToArray();
+        }
+
+        public string Sign(string str)
+        {
+            return privateKey + str;
+        }
+
+        public bool Verify(string signedStr, out byte[] buffer)
+        {
+            buffer = Encoding.UTF8.GetBytes(signedStr.Substring(signedStr.IndexOf(publicKey), publicKey.Length));
+            return true;
+        }
+
+        public string GetHash(string str)
+        {
+            var buffer = Encoding.UTF8.GetBytes(str);
+            return BitConverter.ToString(buffer.Where((_, i) => i % 2 == 0).ToArray());
+        }
+
+        // non primary replica
+        // signedCounterViewNumber <- by primary replica
+        public void UpdateView(string signedCounterViewNumber, string encryptedViewKey)
+        {
+            byte[] buffer;
+            if (Verify(signedCounterViewNumber, out buffer) == false) throw new Exception("Invalid signature");
+
+            var counter = BitConverter.ToUInt32(buffer, 0);
+            var viewNumber = BitConverter.ToUInt32(buffer, 4);
+
+            if (counter != counterLatest + 1) throw new Exception("Invalid counter");
+
+            counterLatest = 0;
+            viewNumber++;
+            if (isActive) viewKey = byte.Parse(Decrypt(encryptedViewKey));
+        }
+
+        // used by passive replicas
+        public void VerifyCounter(
+            string signedXCounterViewNumber,
+            byte[] encryptedReplicaSecret,
+            out string secretShare,
+            out string childrenSecretHash,
+            out string secretHash)
+        {
+            byte[] buffer;
+            if (Verify(signedXCounterViewNumber, out buffer) == false) throw new Exception("Invalid signature");
+
+            var counter = BitConverter.ToUInt32(buffer, 4);
+            var viewNumber = BitConverter.ToUInt32(buffer, 8);
+
+            using (var memory = new MemoryStream(DecryptAuth(encryptedReplicaSecret)))
+            using (var reader = new BinaryReader(memory))
+            {
+                // var secretShareLength = reader.ReadInt32();
+                secretShare = reader.ReadString();
+                var counter2 = reader.ReadUInt32();
+                var viewNumber2 = reader.ReadUInt32();
+                // var childrenSecretHashLength = reader.ReadString();
+                childrenSecretHash = reader.ReadString();
+                // var secretHashLength = reader.ReadString();
+                secretHash = reader.ReadString();
+
+                if (counter != counter2 || viewNumber == viewNumber2) throw new Exception("Invalid counter value");
+                if (counter != counterLatest + 1) throw new Exception("Invalid counter value");
+
+                counterLatest++;
+            }
+        }
+
+        // by passive replicas
+        public void UpdateCounter(string secret, string signedSecretHashCounterViewNumber)
+        {
+            byte[] buffer;
+            if (Verify(signedSecretHashCounterViewNumber, out buffer) == false) throw new Exception("Invalid signature");
+
+            using (var memory = new MemoryStream(buffer))
+            using (var reader = new BinaryReader(memory))
+            {
+                var secretHash = reader.ReadString();
+                var counter = reader.ReadUInt32();
+                var viewNumber = reader.ReadUInt32();
+
+                if (counter != counterLatest + 1) throw new Exception("Invalid counter value");
+                if (GetHash(secret + counter + viewNumber) != secretHash) throw new Exception("Invalid secret");
+
+                counterLatest++;
+            }
+        }
+
+        // by replica
+        public void ResetCounter(IDictionary<string, string> lAndSignedLHashCounterViewNumbers)
+        {
+            const int f = 2;
+
+            var atLeastFPlus1ConsistentL = lAndSignedLHashCounterViewNumbers
+                .Select(lAndSignedLHashCounterViewNumber =>
+                {
+                    byte[] buffer;
+                    if (Verify(lAndSignedLHashCounterViewNumber.Value, out buffer) == false) throw new Exception("Invalid signature");
+
+                    using (var memory = new MemoryStream(buffer))
+                    using (var reader = new BinaryReader(memory))
+                    {
+                        var LHash = reader.ReadString();
+                        var counter = reader.ReadUInt32();
+                        var viewNumber = reader.ReadUInt32();
+
+                        return new
+                        {
+                            L = lAndSignedLHashCounterViewNumber.Key,
+                            counter,
+                            viewNumber
+                        };
+                    }
+                })
+                .GroupBy(x => x.L + x.counter + x.viewNumber)
+                .Where(gx => gx.Count() >= f)
+                .OrderByDescending(gx => gx.Count())
+                .First().First();
+
+            if (atLeastFPlus1ConsistentL != null)
+            {
+                counterLatest = atLeastFPlus1ConsistentL.counter;
+                viewNumber = atLeastFPlus1ConsistentL.viewNumber;
+            }
+        }
+    }
+}
